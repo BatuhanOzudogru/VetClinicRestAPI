e92970aa81688b09d0874f1e08c56824d6c34187
BatuhanOzudogru <batuhan.ozudogru@hotmail.com>
2023-12-05 18:10:45 +0300

    Initial commit

---
+++ src/test/java/dev/patika/VetClinicRestAPI/VetClinicRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patika.VetClinicRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetClinicRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
