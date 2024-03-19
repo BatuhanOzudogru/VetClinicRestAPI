3107695ab63c62d6fa3556b7af23e124dac32f9f
BatuhanOzudogru <batuhan.ozudogru@hotmail.com>
2024-03-19 23:33:17 +0300

    Change web config url

--- src/main/java/dev/patika/config/WebConfig.java
+++ src/main/java/dev/patika/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://vet-app-batuhanozudogrus-projects.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
